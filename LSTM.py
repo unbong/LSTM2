2331a832c811248f3102f184b2962ba8188c7a75
unbong <unbong@unbong-no-MacBook-Pro.local>
2019-04-20 16:11:48 +0900

    second commit

--- LSTM.py
+++ LSTM.py
@@ -33,12 +33,13 @@ class LSTM:
     def backward(self, d_h_next, d_c_next):
         weight_x, weight_h, b = self.params
         forget_mat, input_mat, g_mat, output_mat, h_next, c_next, h_prev, c_prev, x = self.cache
+        
+        tanh_c_next = np.tanh(c_next)
+        d_c_next_calc_by_hidden = d_c_next + (d_h_next * output_mat) * (1 - tanh_c_next**2)
 
-        d_c_prev = d_c_next * forget_mat
-
-        d_g_mat = d_c_next * input_mat
-        d_input_mat = d_c_next * g_mat
-        d_forget_mat = d_c_next * c_prev
+        d_g_mat = d_c_next_calc_by_hidden * input_mat
+        d_input_mat = d_c_next_calc_by_hidden * g_mat
+        d_forget_mat = d_c_next_calc_by_hidden * c_prev
         d_output_mat = d_h_next * np.tanh(c_next)
 
         d_input_mat = d_input_mat * (1-input_mat) * input_mat
@@ -67,10 +68,8 @@ class Time_LSTM:
         self.params=[weight_hs, weight_xs, bs]
         self.grads=[np.zeros_like(weight_hs), np.zeros_like(weight_xs), np.zeros_like(bs)]
         self.layers = None
-        self.h_prev, self.c_prev = None
+        self.h_prev, self.c_prev = None, None
         self.stateful = stateful
-
-
 
     def forward(self, xs ):
 
@@ -78,24 +77,83 @@ class Time_LSTM:
 
         N, T, D = xs.shape
         H = weight_hs.shape[1]
+        hs=np.empty((N, T, H), dtype='f')
 
         self.layers=[]
 
-        if self.stateful == False  or self.h_prev == None:
+        if self.stateful == False or self.h_prev == None:
             self.h_prev = np.zeros((N,H))
 
-        if self.stateful == False or self.c_prev ==None :
+        if self.stateful == False or self.c_prev ==None:
             self.c_prev = np.zeros((N,H))
 
         for i in range(T):
             layer = LSTM(self.params)
-            h_prev , c_prev = layer.forward(xs[:, i, :], h_prev, c_prev )
+            self.h_prev , self.c_prev = layer.forward(xs[:, i, :], h_prev, c_prev )
             self.layers.append(layer)
-            self.h_prev = h_prev
-            self.c_prev = c_prev
+            hs[:, i, :] = self.h_prev
+        return hs
 
-        return self.h_prev, self.c_prev
+    def backward(self, dhs ):
 
-    def backward(self, dhs, dcs):
+        weight_hs, weight_xs, bs = self.params 
+        N, T, H = dhs.shape
+        D = weight_xs.shape[0]
+        dxs = np.empty((N, T, D), dtype='f')
+        dh, dc = 0, 0
+        grads = [0, 0, 0]
 
-        for i
+        for i in reversed(range(T)):
+            layer = self.layers[i]
+            dx, dh, dc = layer.backward(dhs[:, i, :]+dh, dc)
+            dxs[:, i, :] = dx
+            for g, grad in enumerate(layer.grads):
+                grads[g] += grad
+        
+        for i, grad in enumerate(grads):
+            self.grads[i][...] = grad
+        self.dh = dh
+
+        return dxs
+
+
+    def resetstate(self):
+        self.h_prev = None
+        self.c_prev = None
+
+    def setstate(self, cell, hidden):
+        self.h_prev = hidden
+        self.c_prev = cell
+
+class Embeding:
+    def __init__(self, weight, grads):
+        self.weight = weight                    #
+        self.grads = grads   # 导数
+        self.index= None                        # 索引
+
+    def forward(self, index):
+        if index >= self.weight.shape[0]:
+            print(" index > row count")
+            return None
+        self.index = index
+        return self.weight[index]
+
+    def backward(self, ds):
+        np.add.at(self.grads, self.index, ds)
+        return None
+
+
+class TimeEmbedding:
+    def __init__(self, weight):
+        self.weight=weight
+        self.grads=np.zeros_like(self.weight)
+
+
+    def forward(self, xs):
+        return None
+
+    def backward(self, dout):
+        return None
+
+
+
